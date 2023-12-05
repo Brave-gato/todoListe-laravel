b72e27a21bd87701990ff012b6d0a77f194badd8
Gato Dev <webrepoleved@gmail.com>
2023-12-05 16:14:47 +0100

    ajout index blade

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -1,6 +1,23 @@
-<h1>Liste des tâches</h1>
-<ul>
-    @foreach ($tasks as $task)
-        <li>{{ $task->name }}</li>
-    @endforeach
-</ul>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h1>Liste des tâches</h1>
+                    <ul>
+                        @foreach ($tasks as $task)
+                            <li>{{ $task->name }}</li>
+                        @endforeach
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
